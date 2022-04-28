52f0b5502247c1cefdf249f069b56a9d7d8c2e10
Jacksonckc <jackson858216047@gmail.com>
2022-04-28 08:19:28 -0600

    I added the condition on if cartItem didn't have anything, then do not run the logic of mapping the cartItems and putting it onto the DOM

--- src/js/cart.js
+++ src/js/cart.js
@@ -5,9 +5,14 @@ function getLocalStorage(key) {
 function getCartContents() {
   let markup = "";
   const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => renderCartItem(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
-  // document.querySelector(".product-list").innerHTML = renderCartItem(cartItems);
+  cartItems &&
+    (() => {
+      const htmlItems = cartItems.map((item) => renderCartItem(item));
+      document.querySelector(".product-list").innerHTML = htmlItems.join("");
+      document.querySelector(".product-list").innerHTML = renderCartItem(
+        cartItems
+      );
+    });
 }
 
 function renderCartItem(item) {
