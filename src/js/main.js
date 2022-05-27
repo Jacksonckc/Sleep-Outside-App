2351b45c7b0e037ca9cbc2e8ab5660396a43d814
= <=>
2022-05-27 12:36:06 -0600

    Finished Team Activity 5

--- src/js/main.js
+++ src/js/main.js
@@ -7,12 +7,12 @@ loadHeaderFooter();
 const superScript = new CartIconSuperscript();
 superScript.addSuperscript();
 
-// first create an instance of our ProductData class.
-const dataSource = new ProductData("tents");
-// then get the element we want the product list to render in
-const listElement = document.querySelector(".product-list");
-// console.log(listElement);
-// then create an instance of our ProductList class and send it the correct information.
-const myList = new ProductList("tents", dataSource, listElement);
-// finally call the init method to show our products
-myList.init();
+// // first create an instance of our ProductData class.
+// const dataSource = new ProductData("tents");
+// // then get the element we want the product list to render in
+// const listElement = document.querySelector(".product-list");
+// // console.log(listElement);
+// // then create an instance of our ProductList class and send it the correct information.
+// const myList = new ProductList("tents", dataSource, listElement);
+// // finally call the init method to show our products
+// myList.init();
