5f87b7bfa4c5f2c9bc0a5d5d99fdf2cf243b0893
Jacksonckc <jackson858216047@gmail.com>
2022-05-31 09:38:31 -0600

    We are not reading the products on the main page anymore, so no need for these

--- src/js/product.js
+++ src/js/product.js
@@ -1,14 +1,6 @@
-import ProductData from "./productData.js";
-import ProductDetails from "./productDetails.js";
-import { getParams, loadHeaderFooter } from "./utils.js";
+import { loadHeaderFooter } from "./utils.js";
 
 import addSuperscript from "./cartIconSuperscript.js";
-
-const productId = getParams("product");
-const dataSource = new ProductData("tents");
-
-const product = new ProductDetails(productId, dataSource);
-product.init();
 
 const superscript = new addSuperscript();
 superscript.addSuperscript();
