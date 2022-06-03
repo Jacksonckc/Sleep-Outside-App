164ff93bc5c3dc13e13467e0b330a2aca56c8b90
isaacpradford <radf.isaac@gmail.com>
2022-06-03 12:09:53 -0600

    Team Activity 6 initial commit

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,5 +1,6 @@
 import { loadHeaderFooter } from "./utils";
 import addSuperscript from "./cartIconSuperscript";
+import CheckoutProcess from "./CheckoutProcess";
 
 loadHeaderFooter();
 const superscript = new addSuperscript();
