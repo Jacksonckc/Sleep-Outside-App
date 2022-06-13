3a2a1676cbcc7ebb73620eb46abba1ca28baffe2
Jacksonckc <jackson858216047@gmail.com>
2022-06-13 16:37:10 -0600

    clean up

--- src/js/checkoutResult.js
+++ src/js/checkoutResult.js
@@ -1,11 +1,6 @@
 import CartIconSuperscript from "./cartIconSuperscript";
 import { loadHeaderFooter } from "./utils.js";
-import CallToActionModal from "./callToActionModal";
 
 loadHeaderFooter();
 const superScript = new CartIconSuperscript();
 superScript.addSuperscript();
-const callToActionModal = new CallToActionModal(
-  document.getElementById("callToActionModal")
-);
-callToActionModal.init();
